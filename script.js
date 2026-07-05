378ca1d327b86ae83ba273087c108a5f5ac109fe
Piyush Bhati <114479451+Piyush0bhati@users.noreply.github.com>
2026-07-05 17:06:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		this.name = name
+		this.age = age
+	}
+	getName(){
+		return this.name
+	}
+	setAge(age){
+		
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	super()
+	study(){
+		console.log(this.name, "is studing")
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	super()
+	teach(){
+		console.log(this.name, "is teaching")
+	}
+}
+
+const person = new Person("John", 25);
+console.log(person.name); 
 
 // Do not change the code below this line
 window.Person = Person;
